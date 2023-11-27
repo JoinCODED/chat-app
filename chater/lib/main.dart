45df6d1635d8d027ac0735d8e68b494b11117092
ashraf zarkani <ashrafzarkani@gmail.com>
2023-11-27 16:29:47 +0300

    firebase integration

--- chater/lib/main.dart
+++ chater/lib/main.dart
@@ -1,6 +1,15 @@
+import 'package:chater/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  /// ensures that the framework is ready to handle
+  /// widget-related operations and provides access
+  /// to platform-specific resources
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
