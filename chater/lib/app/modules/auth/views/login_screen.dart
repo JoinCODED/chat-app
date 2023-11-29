d1e94ce0bb326424ba9cd579da4444194ac29087
ashraf zarkani <ashrafzarkani@gmail.com>
2023-11-29 18:02:34 +0300

    push cleaning

--- chater/lib/app/modules/auth/views/login_screen.dart
+++ chater/lib/app/modules/auth/views/login_screen.dart
@@ -10,29 +10,3 @@ class LoginScreen extends StatelessWidget {
     );
   }
 }
-
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            context.showSnackbar('Button clicked!');
-          },
-          child: const Text('Show Snackbar'),
-        ),
-      ),
-    );
-  }
-}
-
-extension ContextExtension on BuildContext {
-  void showSnackbar(String message) {
-    ScaffoldMessenger.of(this).showSnackBar(
-      SnackBar(content: Text(message)),
-    );
-  }
-}
