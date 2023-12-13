38e12dc4b7f456bda3b8bb5c2c1dd95f0f7c8ad3
ashraf zarkani <ashrafzarkani@gmail.com>
2023-12-13 17:16:41 +0300

    push bottom navigation setup

--- chater/lib/app/config/router/named_routes.dart
+++ chater/lib/app/config/router/named_routes.dart
@@ -1,10 +1,9 @@
 abstract class MyNamedRoutes {
   static const root = '/';
   static const login = 'login';
-  static const chats = 'chats';
   static const chatDetails = 'chatDetails';
   static const profile = 'profile';
-  static const editProfile = 'editProfile';
   static const homePage = 'homePage';
   static const register = 'register';
+  static const locations = 'locations';
 }
--- chater/lib/app/config/router/routes.dart
+++ chater/lib/app/config/router/routes.dart
@@ -2,11 +2,18 @@ import 'package:chater/app/config/router/named_routes.dart';
 import 'package:chater/app/modules/home/views/home_screen.dart';
 import 'package:chater/app/modules/auth/views/register_screen.dart';
 import 'package:chater/app/modules/auth/views/splash_screen.dart';
+import 'package:chater/app/modules/locations/views/locations.dart';
+import 'package:chater/app/modules/navbar/view/navbar_screen.dart';
+import 'package:chater/app/modules/navbar/widgets/bottom_navbar_tabs.dart';
+import 'package:chater/app/modules/profile/view/profile.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 ///[rootNavigatorKey] used for global | general navigation
 final rootNavigatorKey = GlobalKey<NavigatorState>();
+
+///[shellNavigatorKey] used for nesting routes within bottom nav bar (stickey bottom nav bar)
+final shellNavigatorKey = GlobalKey<NavigatorState>();
 
 class AppRouter {
   static Widget errorWidget(BuildContext context, GoRouterState state) =>
@@ -36,14 +43,43 @@ class AppRouter {
           child: RegisterScreen(),
         ),
       ),
-      GoRoute(
-        parentNavigatorKey: rootNavigatorKey,
-        path: "/${MyNamedRoutes.homePage}",
-        name: MyNamedRoutes.homePage,
-        pageBuilder: (context, state) => NoTransitionPage(
-          key: state.pageKey,
-          child: const HomePage(),
-        ),
+      ShellRoute(
+        navigatorKey: shellNavigatorKey,
+        builder: (context, state, child) {
+          return ScaffoldWithBottomNavBar(
+            tabs: BottomNavBarTabs.tabs(context),
+            child: child,
+          );
+        },
+        routes: [
+          // home
+          GoRoute(
+            path: "/${MyNamedRoutes.homePage}",
+            name: MyNamedRoutes.homePage,
+            pageBuilder: (context, state) => NoTransitionPage(
+              key: state.pageKey,
+              child: const HomePage(),
+            ),
+          ),
+          // locations
+          GoRoute(
+            path: "/${MyNamedRoutes.locations}",
+            name: MyNamedRoutes.locations,
+            pageBuilder: (context, state) => NoTransitionPage(
+              key: state.pageKey,
+              child: const LocationsScreen(),
+            ),
+          ),
+          // profile
+          GoRoute(
+            path: "/${MyNamedRoutes.profile}",
+            name: MyNamedRoutes.profile,
+            pageBuilder: (context, state) => NoTransitionPage(
+              key: state.pageKey,
+              child: const ProfileScreen(),
+            ),
+          ),
+        ],
       ),
     ],
     errorBuilder: errorWidget,
